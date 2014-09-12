49235b49bb299ea2fe9ff8db00222ce961506213
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-09-12 15:38:24 +0300

    Path to images directory needed.

--- task-6.js
+++ task-6.js
@@ -1,6 +1,10 @@
 var imgPath = process.argv[2];
 var createThumbnails = require('./task-5.js');
 
-if (!createThumbnails(imgPath)) {
-    console.log("Given directory does not exist.");
+if (imgPath) {
+    if (!createThumbnails(imgPath)) {
+        console.log("Given directory does not exist.");
+    }
+} else {
+    console.log("Give path to images directory");
 }
